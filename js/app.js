3f939bb56a02d5279ec467437b689d1ed133a469
Thiago-S-Sousa <Thiago_S_Sousa@outlook.com>
2025-09-06 12:23:26 -0300

    feat: app start - initial commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const currentYear = new Date().getFullYear();
+const owner = "CALLART - Software House";
+const text = `${owner}, copyright © ${currentYear} todos os direitos reservados.`;
+document.getElementById("footer-text").textContent = text;
+
+// ------ FOOTER ------ //
